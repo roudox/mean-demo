76635861bb3b9ed166af7e5ea826be4369f83116
ngoolch <nicholas.goolcharan@loblaw.ca>
2015-02-01 09:46:18 -0500

    added readme doc

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
-  res.sendfile(__dirname + '/client/views/index.html');
+  res.sendFile(__dirname + '/client/views/index.html');
 });
 
 app.use('/js', express.static(__dirname + '/client/js'));
