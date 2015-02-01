baba4cd73ef399015f7d75c631cdfb8cea9a139a
ngoolch <nicholas.goolcharan@loblaw.ca>
2015-01-31 23:47:50 -0500

    added meetupsController

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.get('/', function(req, res) {
   res.sendfile(__dirname + '/client/views/index.html');
 });
 
+app.use('/js', express.static(__dirname + '/client/js'));
+
 app.listen(3000, function() {
   console.log('I\'m Listening...');
 });
